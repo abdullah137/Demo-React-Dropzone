9a0d6e554fd9e63fca9728c89ec8e7d4ea2950b9
abdullah137 <abdullahyahaya46@gmail.com>
2023-04-14 15:03:13 +0100

    making some comit

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [images, setImages] = useState([]);
+
+  function handleUpload() {
+    console.log('uploading file...');
+    axios.post('http://localhost:4000/upload', {images}).then(response => {
+      console.log(response.data);
+    }).catch(error => {
+      console.error(error.message);
+    })
+  }
+
+  const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+    
+    acceptedFiles.forEach(file => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImages(prevState => [...prevState, {...file, buffer: reader.result}]);
+      }
+      reader.readAsDataURL(file);
+    })
+    
+  }, []);
+
+  useEffect(() => {
+    console.log(images)
+  }, [images]);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: ['image/png', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'] });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='dropzone'  { ...getRootProps()}>
+        <input { ...getInputProps() } />
+          {isDragActive ? "Drag Active" : "You can drop your files here." }
+      </div>
+      {images.length > 0 && <div> 
+
+          {images.length > 0 && <button onClick={handleUpload}>Upload Images</button>}
+        </div> }
     </div>
   );
 }
